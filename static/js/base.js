07b083155aa578e6e6a9494c112ebfb0a220667b
David Hu <hujinxin0607@gmail.com>
2022-08-15 13:04:11 -0700

    Build player class
    Create and build player class for new player create, and create two play at js/base.js class.

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,29 @@
 import { GameMap } from '/static/js/game_map/base.js';
+import { Player } from '/static/js/player/base.js';
 
 class KOF {
     constructor(id) {
         this.$kof = $('#' + id);
 
         this.game_map = new GameMap(this);
+        this.players = [
+            new Player(this, {
+                id: 0,
+                x: 0,
+                y: 0,
+                width: 150,
+                height: 200,
+                color: 'blue',
+            }),
+            new Player(this, {
+                id: 1,
+                x: 500,
+                y: 0,
+                width: 150,
+                height: 200,
+                color: 'red',
+            }),
+        ];
     }
 }
 
